1dca12051ee763c43cd13868ea68c470d1aae85e
Ilia Zaitsev <git@zaitsev.sh>
2026-05-19 22:59:54 +0200

    feat: domain updated

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 import { dev } from '$app/environment';
 export const title = "Ilia Zaitsev's Blog";
 export const description = 'Make blogging cool again!';
-export const url = dev ? 'http://localhost:5173' : 'https://iliazaitsev.me';
+export const url = dev ? 'http://localhost:5173' : 'https://zaitsev.sh';
